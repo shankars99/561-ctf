427d4f5d7f06f98bd0efd39aa0c94b9ac3555a30
shankars99 <shankars0899@gmail.com>
2024-04-21 21:52:32 -0400

    chore: backup migrate to blockchain simulation
    Signed-off-by: shankars99 <shankars0899@gmail.com>

---
+++ src/blockchain_handler.back.py
@@ -0,0 +1,141 @@
+from web3 import Web3
+from solcx import compile_source
+from hexbytes import HexBytes as hb
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+w3 = Web3(Web3.HTTPProvider('http://localhost:8545'))
+
+
+PUB_KEY_LEADER = config['PUB_KEY_LEADER']
+PUB_KEY_RIGHT_HAND = config['PUB_KEY_RIGHT_HAND']
+PUB_KEY_HERO = config['PUB_KEY_HERO']
+
+PUB_KEY_BOT = config['PUB_KEY_BOT']
+
+contract_src = 'src/contracts/'
+
+hackers = [PUB_KEY_LEADER, PUB_KEY_RIGHT_HAND, PUB_KEY_HERO]
+# Contract state information, used to deploy and then store the contract addresses
+contracts = {
+    'EvilHackerDAO': {
+        'contract': 'EvilHackerDAO',
+        'constructor': [hackers],
+        'address': ''
+    },
+    'EvilHackerWallet': {
+        'contract': 'EvilHackerWallet',
+        'constructor': [],
+        'address': ''
+    }
+}
+
+# Fetches the ETH balance of a public key
+def get_balance(public_key):
+    return w3.eth.get_balance(public_key)
+
+# Generates a public key from a private key
+def gen_pub_from_priv(private_key):
+    return Web3.to_checksum_address(w3.eth.account.from_key(private_key).address)
+
+# Reads a contract from a file and returns the contract interface that contains the ABI and bytecode
+def get_contract_interface(contract_name: str):
+    with open(contract_src + contract_name + ".sol", 'r') as f:
+        contract_data = f.read()
+
+    compiled_contract = compile_source(contract_data)
+    contract_id, contract_interface = compiled_contract.popitem()
+    return contract_interface
+
+# Returns a smart contract object using the interface. It then stores and returns the contract address
+def get_w3_contract(contract_name: str):
+    contract_interface = get_contract_interface(contract_name)
+    return w3.eth.contract(address=contracts[contract_name]['address'], abi=contract_interface['abi'])
+
+# Deploys a contract (from the account bot) and returns the contract address
+def deploy_contract(contract_name):
+    contract = contracts[contract_name]['contract']
+    args = contracts[contract_name]['constructor']
+
+    contract_interface = get_contract_interface(contract)
+
+    tx_hash = w3.eth.contract(
+            abi=contract_interface['abi'],
+            bytecode=contract_interface['bin']).constructor(*args).transact({'from': PUB_KEY_BOT})
+
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    address = w3.eth.get_transaction_receipt(tx_hash)['contractAddress']
+    contracts[contract_name]['address'] = address
+    return address
+
+# Deploys all contracts and writes the addresses to a json file
+def deploy_contracts():
+    contracts = ['EvilHackerDAO', 'EvilHackerWallet']
+
+    address_DAO = deploy_contract(contracts[0])
+    address_wallet = deploy_contract(contracts[1])
+
+    addresses = [address_DAO, address_wallet]
+
+    # Write the addresses as {contract_name: address} to a json file
+    with open('src/contracts.json', 'w') as f:
+        f.write('{\n')
+        for i in range(len(contracts)):
+            f.write(f'"{contracts[i]}": "{addresses[i]}"')
+            if i != len(contracts) - 1:
+                f.write(',\n')
+        f.write('\n}')
+
+    return addresses
+
+# Transfers hacker tokens between two addresses. Transaction is signed by the bot
+def transfer_tokens(from_address: str, to_address: str, token_value: int):
+    contract = 'EvilHackerDAO'
+
+    EvilHackerDao = get_w3_contract(contract)
+    tx_hash = EvilHackerDao.functions.transfer(from_address, to_address, token_value).transact({'from': PUB_KEY_BOT})
+
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    return hb.hex(tx_hash)
+
+# Updates the owner of the EvilHackerDAO contract. Transaction is signed by the bot
+def update_owner():
+    contract = 'EvilHackerDAO'
+
+    EvilHackerDao = get_w3_contract(contract)
+    tx_hash = EvilHackerDao.functions.updateOwner().transact({'from': PUB_KEY_BOT})
+
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    return hb.hex(tx_hash)
+
+# Returns the hacker token balance of a public key in the EvilHackerDAO contract
+def get_hacker_balance(public_key):
+    contract = 'EvilHackerDAO'
+
+    EvilHackerDao = get_w3_contract(contract)
+    balance = EvilHackerDao.functions.getHackerBalance(public_key).call()
+
+    return balance
+
+# Returns the owner of the EvilHackerDAO contract
+def get_hacker_owner():
+    contract = 'EvilHackerDAO'
+
+    EvilHackerDao = get_w3_contract(contract)
+    owner = EvilHackerDao.functions.owner().call()
+
+    return owner
+
+# Deposits ETH into the EvilHackerWallet contract. Transaction is signed by the bot
+def deposit_to_wallet(value: int):
+    contract = 'EvilHackerWallet'
+
+    EvilHackerWallet = get_w3_contract(contract)
+    tx_hash = EvilHackerWallet.functions.deposit().transact({'from': PUB_KEY_BOT, 'value': value * 10**18})
+
+    w3.eth.wait_for_transaction_receipt(tx_hash)
+
+    return hb.hex(tx_hash)
---
+++ src/main.back.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template
+from paillier_crypto import Paillier
+import blockchain_handler as bh
+from dotenv import dotenv_values
+config = dotenv_values(".env")
+
+app = Flask(__name__)
+
+approved_keys = [config['PRIV_KEY_LEADER'], config['PRIV_KEY_RIGHT_HAND']]
+
+paillier_keys = Paillier()
+# bh.deploy_contracts()
+# print(bh.deposit_to_wallet(100))
+
+# curl http://127.0.0.1:5000
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# curl http://127.0.0.1:5000/game
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+# curl http://127.0.0.1:5000/hint
+@app.route('/hint')
+def hint():
+    return render_template('hint.html')
+
+# curl http://127.0.0.1:5000/info
+@app.route('/info')
+def hacker_info():
+    return render_template('info.html')
+
+# curl http://127.0.0.1:5000/encrypt/10
+@app.route('/encrypt/<int:tokens>')
+def encrypt(tokens):
+    return str(paillier_keys.encrypt(tokens))
+
+# curl http://127.0.0.1:5000/decrypt/$PRIV_KEY_RIGHT_HAND/${ENCRYPTED_VALUE}
+@app.route('/decrypt/<string:key>/<int:encrypted_value>')
+def dec_tokens(key, encrypted_value):
+    if key not in approved_keys:
+        return 'Invalid key'
+
+    token_value = paillier_keys.decrypt(encrypted_value)
+    return str(token_value)
+
+# curl http://127.0.0.1:5000/balance/$PUB_KEY_HERO
+@app.route('/balance/<string:address>')
+def hacker_balance(address):
+    hacker_balance = bh.get_hacker_balance(address)
+    return str(hacker_balance)
+
+# curl http://127.0.0.1:5000/ethbalance/0x57C75A6B1EE0F9698386B9FE674A7B3Eb45E9A4B
+@app.route('/ethbalance/<string:address>')
+def eth_balance(address):
+    eth_balance = bh.get_balance(address)
+    return str(eth_balance)
+
+
+# curl http://127.0.0.1:5000/owner
+@app.route('/owner')
+def get_owner():
+    hacker_owner = bh.get_hacker_owner()
+    return str(hacker_owner)
+
+# curl http://127.0.0.1:5000/trade/$PRIV_KEY_RIGHT_HAND/$PUB_KEY_HERO/${ENCRYPTED_VALUE}
+@app.route('/trade/<string:from_key>/<string:to_address>/<int:encrypted_token_value>')
+def trade(from_key, to_address, encrypted_token_value):
+    token_value = int(dec_tokens(from_key, encrypted_token_value))
+    from_address = bh.gen_pub_from_priv(from_key)
+    tx_hash= bh.transfer_tokens(from_address, to_address, token_value)
+    return tx_hash
+
+# curl http://127.0.0.1:5000/update
+@app.route('/update')
+def update_owner():
+    tx_hash = bh.update_owner()
+
+    return tx_hash
+
+if __name__ == '__main__':
+    app.run()
